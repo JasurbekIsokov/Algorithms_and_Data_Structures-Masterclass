de5cd120d7ba5e23929adfc7aa3122c7e1da6b76
Jasurbek Isokov <jasurbekisokov@Jasurbeks-MacBook-Pro.local>
2023-06-27 10:54:34 +0500

    Merge Sort

--- script.js
+++ script.js
@@ -503,3 +503,55 @@
 // }
 
 // console.log(bubbleSort([1, 2, 4, 3, 7, 6, 8, 90]));
+
+// ------------------------------------------------------------
+
+// Intermediate Sorting Algorithms
+
+// Merge sort introduction
+
+// Merging arrays
+
+function mergingArrays(arr1, arr2) {
+  let result = [];
+  let j = 0;
+  let i = 0;
+
+  while (i < arr1?.length && j < arr2?.length) {
+    if (arr2[j] > arr1[i]) {
+      result.push(arr1[i]);
+      i++;
+    } else {
+      result.push(arr2[j]);
+      j++;
+    }
+  }
+
+  while (i < arr1?.length) {
+    result.push(arr1[i]);
+    i++;
+  }
+
+  while (j < arr2?.length) {
+    result.push(arr2[j]);
+    j++;
+  }
+
+  return result;
+}
+
+// console.log(mergingArrays([100, 200], [1, 2, 3, 5, 6]));
+
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+
+  let middle = Math.floor(arr.length / 2);
+
+  let left = mergeSort(arr.slice(0, middle));
+
+  let right = mergeSort(arr.slice(middle));
+
+  return mergingArrays(left, right);
+}
+
+console.log(mergeSort([2, 1, 6, 7, 2, 55, 21, 60, 25, 17]));
