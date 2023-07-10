ca88917c26dd9d954229599b3fc1043289421739
Jasurbek Isokov <jasurbekisokov@Jasurbeks-MacBook-Pro.local>
2023-07-10 11:32:38 +0500

    Linket List revorce method

--- script.js
+++ script.js
@@ -906,6 +906,22 @@ class SinglyList {
 
     return removed;
   }
+
+  // reverse
+  reverse() {
+    var node = this.head;
+    this.head = this.tail;
+    this.tail = node;
+    var next;
+    var prev = null;
+    for (var i = 0; i < this.length; i++) {
+      next = node.next;
+      node.next = prev;
+      prev = node;
+      node = next;
+    }
+    return this;
+  }
 }
 
 const list = new SinglyList();
