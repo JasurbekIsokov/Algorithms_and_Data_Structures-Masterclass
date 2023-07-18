d3159f35c36418134badb70906cdf718760628b9
Jasurbek Isokov <jasurisoqov685@gmail.com>
2023-07-18 08:33:53 +0500

    Double Linked List pop method

--- script.js
+++ script.js
@@ -967,17 +967,32 @@ class DoublyLinkedList {
     this.length = 0;
   }
 
-  push(val){
+  push(val) {
     var newNode = new Node(val);
-    if(this.length === 0){
-        this.head = newNode;
-        this.tail = newNode;
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
     } else {
-        this.tail.next = newNode;
-        newNode.prev = this.tail;
-        this.tail = newNode;
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
     }
     this.length++;
     return this;
-}
+  }
+
+  pop() {
+    if (!this.head) return undefined;
+    var poppedNode = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = poppedNode.prev;
+      this.tail.next = null;
+      poppedNode.prev = null;
+    }
+    this.length--;
+    return poppedNode;
+  }
 }
