ce7204c0216c1c290a352deddce4e9ea26288634
Jasurbek Isokov <jasurisoqov685@gmail.com>
2023-07-18 08:33:09 +0500

    Double Linked List push method

--- script.js
+++ script.js
@@ -966,4 +966,18 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  push(val){
+    var newNode = new Node(val);
+    if(this.length === 0){
+        this.head = newNode;
+        this.tail = newNode;
+    } else {
+        this.tail.next = newNode;
+        newNode.prev = this.tail;
+        this.tail = newNode;
+    }
+    this.length++;
+    return this;
+}
 }
