79e5221403c94e39e0baf761cb088aea91ef3e9b
Jasurbek Isokov <jasurisoqov685@gmail.com>
2023-07-18 08:32:20 +0500

    Introduction Double Linked List

--- script.js
+++ script.js
@@ -743,207 +743,227 @@
 
 // Singly Linked List
 
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.nextPointer = null;
+//   }
+// }
+
+// class SinglyList {
+//   constructor() {
+//     this.head = null;
+//     this.tail = null;
+//     this.length = 0;
+//   }
+
+//   // oxiridan qo'shish
+//   push(val) {
+//     const newNode = new Node(val);
+//     if (!this.head) {
+//       this.head = newNode;
+//       this.tail = this.head;
+//     } else {
+//       this.tail.next = newNode;
+//       this.tail = newNode;
+//     }
+
+//     this.length++;
+//     return this;
+//   }
+
+//   // oxiridan o'chirish
+//   pop() {
+//     if (!this.tail) return undefined;
+//     var current = this.head;
+//     var newTail = current;
+
+//     while (current.next) {
+//       newTail = current;
+//       current = current.next;
+//     }
+
+//     this.tail = newTail;
+//     this.tail.next = null;
+//     this.length--;
+
+//     if (this.length === 0) {
+//       this.head = null;
+//       this.tail = null;
+//     }
+
+//     return current;
+//   }
+
+//   // boshidan o'chirish
+//   shift() {
+//     if (!this.head) return undefined;
+
+//     var current = this.head;
+//     this.head = current.next;
+//     this.length--;
+//     if (this.length === 0) {
+//       this.head = null;
+//       this.tail = null;
+//     }
+
+//     return current;
+//   }
+
+//   // boshidan qo'shish
+//   unShift(val) {
+//     const newNode = new Node(val);
+
+//     if (!this.head) {
+//       this.head = newNode;
+//       this.tail = this.head;
+//     } else {
+//       newNode.next = this.head;
+//       this.head = newNode;
+//     }
+
+//     this.length++;
+//     return this;
+//   }
+
+//   // get elementning indexi orqali uni olish
+//   get(index) {
+//     if (index < 0 || this.length <= index) return null;
+
+//     var counter = 0;
+//     var currentElement = this.head;
+
+//     while (counter != index) {
+//       counter++;
+
+//       currentElement = currentElement.next;
+//     }
+
+//     return currentElement;
+//   }
+
+//   // set => elementni o'zgartirish
+//   set(index, value) {
+//     var getElement = this.get(index);
+
+//     if (getElement) {
+//       getElement.val = value;
+//     }
+
+//     return this;
+//   }
+
+//   // element qo'shish
+//   insert(index, value) {
+//     if (index < 0 || index > this.length) return false;
+
+//     if (index === this.length) {
+//       this.push(value);
+
+//       return true;
+//     }
+
+//     if (index === 0) {
+//       this.unShift(value);
+
+//       return true;
+//     }
+
+//     var newNode = new Node(value);
+
+//     var element = this.get(index - 1);
+
+//     var temp = element.next;
+
+//     newNode.next = temp;
+
+//     element.next = newNode;
+
+//     this.length++;
+
+//     return true;
+//   }
+
+//   // element o'chirish
+//   remove(index) {
+//     if (index < 0 || index >= this.length) return undefined;
+
+//     if (index == this.length - 1) {
+//       return this.pop();
+//     }
+
+//     if ((index = 0)) {
+//       return this.shift();
+//     }
+
+//     var previousNode = this.get(index - 1);
+
+//     var removed = previousNode.next;
+
+//     previousNode.next = removed.next;
+
+//     this.length--;
+
+//     return removed;
+//   }
+
+//   // reverse
+//   reverse() {
+//     var node = this.head;
+//     this.head = this.tail;
+//     this.tail = node;
+//     var next;
+//     var prev = null;
+//     for (var i = 0; i < this.length; i++) {
+//       next = node.next;
+//       node.next = prev;
+//       prev = node;
+//       node = next;
+//     }
+//     return this;
+//   }
+// }
+
+// const list = new SinglyList();
+
+// list.push("First Name");
+
+// list.push("Last Name");
+
+// list.push("Full Name");
+
+// // list.pop();
+
+// // console.log(list.shift());
+
+// // list.unShift("Email");
+
+// // console.log(list.get(2));
+
+// // console.log(list.set(2,"New Value"));
+
+// // list.insert(2, "Phone Number");
+
+// list.remove(2);
+
+// console.log(list);
+
+// ------------------------------------------------------------------------------------------------------------------------
+
+// Double Linked List
+
 class Node {
   constructor(val) {
     this.val = val;
-    this.nextPointer = null;
+    this.next = null;
+    this.prev = null;
   }
 }
 
-class SinglyList {
+class DoublyLinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
     this.length = 0;
   }
-
-  // oxiridan qo'shish
-  push(val) {
-    const newNode = new Node(val);
-    if (!this.head) {
-      this.head = newNode;
-      this.tail = this.head;
-    } else {
-      this.tail.next = newNode;
-      this.tail = newNode;
-    }
-
-    this.length++;
-    return this;
-  }
-
-  // oxiridan o'chirish
-  pop() {
-    if (!this.tail) return undefined;
-    var current = this.head;
-    var newTail = current;
-
-    while (current.next) {
-      newTail = current;
-      current = current.next;
-    }
-
-    this.tail = newTail;
-    this.tail.next = null;
-    this.length--;
-
-    if (this.length === 0) {
-      this.head = null;
-      this.tail = null;
-    }
-
-    return current;
-  }
-
-  // boshidan o'chirish
-  shift() {
-    if (!this.head) return undefined;
-
-    var current = this.head;
-    this.head = current.next;
-    this.length--;
-    if (this.length === 0) {
-      this.head = null;
-      this.tail = null;
-    }
-
-    return current;
-  }
-
-  // boshidan qo'shish
-  unShift(val) {
-    const newNode = new Node(val);
-
-    if (!this.head) {
-      this.head = newNode;
-      this.tail = this.head;
-    } else {
-      newNode.next = this.head;
-      this.head = newNode;
-    }
-
-    this.length++;
-    return this;
-  }
-
-  // get elementning indexi orqali uni olish
-  get(index) {
-    if (index < 0 || this.length <= index) return null;
-
-    var counter = 0;
-    var currentElement = this.head;
-
-    while (counter != index) {
-      counter++;
-
-      currentElement = currentElement.next;
-    }
-
-    return currentElement;
-  }
-
-  // set => elementni o'zgartirish
-  set(index, value) {
-    var getElement = this.get(index);
-
-    if (getElement) {
-      getElement.val = value;
-    }
-
-    return this;
-  }
-
-  // element qo'shish
-  insert(index, value) {
-    if (index < 0 || index > this.length) return false;
-
-    if (index === this.length) {
-      this.push(value);
-
-      return true;
-    }
-
-    if (index === 0) {
-      this.unShift(value);
-
-      return true;
-    }
-
-    var newNode = new Node(value);
-
-    var element = this.get(index - 1);
-
-    var temp = element.next;
-
-    newNode.next = temp;
-
-    element.next = newNode;
-
-    this.length++;
-
-    return true;
-  }
-
-  // element o'chirish
-  remove(index) {
-    if (index < 0 || index >= this.length) return undefined;
-
-    if (index == this.length - 1) {
-      return this.pop();
-    }
-
-    if ((index = 0)) {
-      return this.shift();
-    }
-
-    var previousNode = this.get(index - 1);
-
-    var removed = previousNode.next;
-
-    previousNode.next = removed.next;
-
-    this.length--;
-
-    return removed;
-  }
-
-  // reverse
-  reverse() {
-    var node = this.head;
-    this.head = this.tail;
-    this.tail = node;
-    var next;
-    var prev = null;
-    for (var i = 0; i < this.length; i++) {
-      next = node.next;
-      node.next = prev;
-      prev = node;
-      node = next;
-    }
-    return this;
-  }
 }
-
-const list = new SinglyList();
-
-list.push("First Name");
-
-list.push("Last Name");
-
-list.push("Full Name");
-
-// list.pop();
-
-// console.log(list.shift());
-
-// list.unShift("Email");
-
-// console.log(list.get(2));
-
-// console.log(list.set(2,"New Value"));
-
-// list.insert(2, "Phone Number");
-
-list.remove(2);
-
-console.log(list);
