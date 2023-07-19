26b2ad5eeceebf7d710ce857ea64f47de2d779f8
Jasurbek Isokov <jasurisoqov685@gmail.com>
2023-07-19 09:14:00 +0500

    Data Structure Queues enqueue and dequeue method

---
+++ queue.js
@@ -0,0 +1,87 @@
+// Stack and Queues
+
+class Stack {
+  constructor() {
+    this.stack = [];
+  }
+
+  push(item) {
+    this.stack.push(item);
+  }
+
+  pop() {
+    // if (this.isEmpty()) {
+    //   return "Stack bo'sh";
+    // }
+    return this.stack.pop();
+  }
+
+  isEmpty() {
+    return this.stack.length === 0;
+  }
+
+  peek() {
+    // if (this.isEmpty()) {
+    //   return "Stack Bo'sh";
+    // }
+
+    return this.stack[this.stack.length - 1];
+  }
+
+  size() {
+    return this.stack.length;
+  }
+}
+
+// const myStack = new Stack();
+
+// myStack.push(12);
+
+// myStack.push(13);
+
+// myStack.push(14);
+
+// myStack.pop();
+
+// console.log(myStack.isEmpty());
+
+// console.log(myStack);
+
+// console.log(myStack.peek());
+
+class Queues {
+  constructor() {
+    this.first = new Stack();
+    this.second = new Stack();
+  }
+
+  enqueue(val) {
+    this.first.push(val);
+  }
+
+  dequeue() {
+    while (this.first.peek()) {
+      this.second.push(this.first.pop());
+    }
+
+    const record = this.second.pop();
+
+    while (this.second.peek()) {
+      this.first.push(this.second.pop());
+    }
+
+    return record;
+  }
+}
+
+const myQueues = new Queues();
+
+myQueues.enqueue("A");
+myQueues.enqueue("B");
+myQueues.enqueue("C");
+myQueues.enqueue("D");
+myQueues.enqueue("E");
+myQueues.enqueue("F");
+myQueues.enqueue("J");
+
+console.log(myQueues);
